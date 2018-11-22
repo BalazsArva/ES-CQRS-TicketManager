5102c83b78010ed03f7034c4a7f93bc5048f9b40
Balazs Arva <Balazs_Arva@epam.com>
2018-11-22 11:48:56 +0100

    Added event DbSets to EventsContext.

--- TicketManager/TicketManager.DataAccess.Events/EventsContext.cs
+++ TicketManager/TicketManager.DataAccess.Events/EventsContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using TicketManager.DataAccess.Events.DataModel;
 
 namespace TicketManager.DataAccess.Events
 {
@@ -8,5 +9,19 @@ namespace TicketManager.DataAccess.Events
             : base(dbContextOptions)
         {
         }
+
+        public DbSet<TicketCommentEditedEvent> TicketCommentEditedEvents { get; set; }
+
+        public DbSet<TicketCommentPostedEvent> TicketCommentPostedEvents { get; set; }
+
+        public DbSet<TicketCreatedEvent> TicketCreatedEvents { get; set; }
+
+        public DbSet<TicketDetailsChangedEvent> TicketDetailsChangedEvents { get; set; }
+
+        public DbSet<TicketLinkedEvent> TicketLinkedEvents { get; set; }
+
+        public DbSet<TicketStatusChangedEvent> TicketStatusChangedEvents { get; set; }
+
+        public DbSet<TicketTagChangedEvent> TicketTagChangedEvents { get; set; }
     }
 }
