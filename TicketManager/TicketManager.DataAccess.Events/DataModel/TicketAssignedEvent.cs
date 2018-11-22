ea608eff775e157965249d8e7cd332d5c1d0549e
Árva Balázs <balazs.arva@windowslive.com>
2018-11-22 20:37:13 +0100

    Configured indices on date of record columns of events.
    Created TicketAssignedEvent event.
    
    Updated db context and added migration.

---
+++ TicketManager/TicketManager.DataAccess.Events/DataModel/TicketAssignedEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TicketManager.DataAccess.Events.DataModel
+{
+    public class TicketAssignedEvent : EventBase, ITicketEvent
+    {
+        public int TicketCreatedEventId { get; set; }
+
+        [ForeignKey(nameof(TicketCreatedEventId))]
+        public virtual TicketCreatedEvent TicketCreatedEvent { get; set; }
+
+        [MaxLength(256)]
+        public string AssignedTo { get; set; }
+    }
+}
