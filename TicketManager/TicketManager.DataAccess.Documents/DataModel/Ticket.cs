5c718ca26d757a8e7440ccd8faadeb38e8cd6ea3
Balazs Arva <Balazs_Arva@epam.com>
2018-11-22 16:09:52 +0100

    Storing the created ticket in QueryStoreSynchronizer.

---
+++ TicketManager/TicketManager.DataAccess.Documents/DataModel/Ticket.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TicketManager.Domain.Common;
+
+namespace TicketManager.DataAccess.Documents.DataModel
+{
+    public class Ticket
+    {
+        public string Id { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime UtcDateCreated { get; set; }
+
+        public string Title { get; set; }
+
+        public string LastEditedBy { get; set; }
+
+        public string Description { get; set; }
+
+        public Priority Priority { get; set; }
+
+        public TicketType TicketType { get; set; }
+
+        public DateTime UtcDateLastEdited { get; set; }
+
+        public TicketStatus TicketStatus { get; set; }
+    }
+}
---
+++ TicketManager/TicketManager.DataAccess.Documents/Extensions/IAsyncDocumentSessionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Raven.Client.Documents.Session;
+
+namespace TicketManager.DataAccess.Documents.Extensions
+{
+    public static class IAsyncDocumentSessionExtensions
+    {
+        public static string GeneratePrefixedDocumentId<TDocument>(this IAsyncDocumentSession documentSession, TDocument entity, string customIdValue)
+        {
+            var separator = documentSession.Advanced.DocumentStore.Conventions.IdentityPartsSeparator;
+            var collectionName = documentSession.Advanced.DocumentStore.Conventions.GetCollectionName(entity);
+
+            return string.Concat(collectionName, separator, customIdValue);
+        }
+    }
+}
