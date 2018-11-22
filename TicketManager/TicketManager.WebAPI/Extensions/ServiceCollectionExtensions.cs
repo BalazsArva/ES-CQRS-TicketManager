55a77a823a52ac2ffd0de234d89399ebf350105a
Balazs Arva <Balazs_Arva@epam.com>
2018-11-22 14:36:13 +0100

    Extracted EF and RavenDb setup to service collection extension methods.

---
+++ TicketManager/TicketManager.WebAPI/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Raven.Client.Documents;
+using TicketManager.DataAccess.Events;
+
+namespace TicketManager.WebAPI.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddRavenDb(this IServiceCollection services, IConfiguration configuration)
+        {
+            var ravenDbUrls = configuration.GetSection("DataAccess:RavenDb:Urls").Get<string[]>();
+            var ravenDbDatabase = configuration["DataAccess:RavenDb:Database"];
+
+            services.AddSingleton(new DocumentStore
+            {
+                Urls = ravenDbUrls,
+                Database = ravenDbDatabase
+            }.Initialize());
+
+            return services;
+        }
+
+        public static IServiceCollection AddEventsContext(this IServiceCollection services, IConfiguration Configuration)
+        {
+            var sqlConnectionString = Configuration["DataAccess:SQL"];
+
+            var options = new DbContextOptionsBuilder<EventsContext>()
+                .UseSqlServer(sqlConnectionString)
+                .Options;
+
+            services.AddSingleton(options);
+            services.AddDbContext<EventsContext>(opts => opts.UseSqlServer(sqlConnectionString));
+            services.AddSingleton<IEventsContextFactory>(svcProvider =>
+            {
+                return new EventsContextFactory(options);
+            });
+
+            return services;
+        }
+    }
+}
