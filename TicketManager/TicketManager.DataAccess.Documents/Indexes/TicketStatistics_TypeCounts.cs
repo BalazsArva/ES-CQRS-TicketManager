ea8dc58a549fa2431db6aea2d266eaa998f45aa7
BalazsArva <balazs.arva@windowslive.com>
2019-01-24 10:27:12 +0100

    Added Statistics indices: - Count by Type - Count by Priority

---
+++ TicketManager/TicketManager.DataAccess.Documents/Indexes/TicketStatistics_TypeCounts.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using Raven.Client.Documents.Indexes;
+using TicketManager.Contracts.Common;
+using TicketManager.DataAccess.Documents.DataModel;
+
+namespace TicketManager.DataAccess.Documents.Indexes
+{
+    public class TicketStatistics_TypeCounts : AbstractIndexCreationTask<Ticket, TicketStatistics_TypeCounts.IndexEntry>
+    {
+        public class IndexEntry
+        {
+            public TicketTypes Type { get; set; }
+
+            public int Count { get; set; }
+        }
+
+        public TicketStatistics_TypeCounts()
+        {
+            Priority = IndexPriority.High;
+
+            // TODO: Add sprint/iteration support and create index by its value
+            Map = tickets => from t in tickets
+                             select new IndexEntry
+                             {
+                                 Type = t.TicketType.Type,
+                                 Count = 1
+                             };
+
+            Reduce = results => from result in results
+                                group result by result.Type into g
+                                select new IndexEntry
+                                {
+                                    Type = g.Key,
+                                    Count = g.Sum(x => x.Count)
+                                };
+
+            StoreAllFields(FieldStorage.Yes);
+        }
+    }
+}
