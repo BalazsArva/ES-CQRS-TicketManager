d922c3e0fd50758d608efaff25bb8b81bdff9471
BalazsArva <balazs.arva@windowslive.com>
2019-02-10 16:29:29 +0100

    Refactorings.

---
+++ TicketManager/PoC.TicketManager.Messaging.Receiver/MessageReceiver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using PoC.TicketManager.Messaging.Shared;
+using TicketManager.Messaging.Configuration;
+using TicketManager.Messaging.Receivers;
+
+namespace PoC.TicketManager.Messaging.Receiver
+{
+    public class MessageReceiver : ReceiverHost<Message>
+    {
+        public MessageReceiver(ServiceBusSubscriptionConfiguration subscriptionConfiguration) : base(subscriptionConfiguration)
+        {
+        }
+
+        protected override Task HandleMessageAsync(Message message, string correlationId, IDictionary<string, object> headers, CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"Received a message for ticket with Id={message.TicketId} (Correlation Id: {correlationId})");
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- TicketManager/PoC.TicketManager.Messaging.Receiver/Program.cs
+++ TicketManager/PoC.TicketManager.Messaging.Receiver/Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using PoC.TicketManager.Messaging.Shared;
 using TicketManager.Messaging.Configuration;
-using TicketManager.Messaging.Receivers;
 
 namespace PoC.TicketManager.Messaging.Receiver
 {
@@ -34,20 +30,6 @@ namespace PoC.TicketManager.Messaging.Receiver
                 .Build();
 
             await host.RunAsync();
-        }
-    }
-
-    public class MessageReceiver : ReceiverHost<Message>
-    {
-        public MessageReceiver(ServiceBusSubscriptionConfiguration subscriptionConfiguration) : base(subscriptionConfiguration)
-        {
-        }
-
-        protected override Task HandleMessageAsync(Message message, string correlationId, IDictionary<string, object> headers, CancellationToken cancellationToken)
-        {
-            Console.WriteLine($"Received a message for ticket with Id={message.TicketId} (Correlation Id: {correlationId})");
-
-            return Task.CompletedTask;
         }
     }
 }
