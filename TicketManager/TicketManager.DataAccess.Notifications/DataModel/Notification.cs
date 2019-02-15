3d44a3bbb3447f2afa1e5cd22d89ce3225fc49a6
BalazsArva <balazs.arva@windowslive.com>
2019-02-15 15:32:01 +0100

    WIP: Notifications.

---
+++ TicketManager/TicketManager.DataAccess.Notifications/DataModel/Notification.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TicketManager.DataAccess.Notifications.DataModel
+{
+    public class Notification
+    {
+        public long Id { get; set; }
+
+        public string User { get; set; }
+
+        public string SourceSystem { get; set; }
+
+        public DateTime UtcDateTimeCreated { get; set; }
+
+        public string Type { get; set; }
+
+        public string Title { get; set; }
+
+        public string BrowserHref { get; set; }
+
+        public string ResourceHref { get; set; }
+
+        public string IconUri { get; set; }
+
+        public bool IsRead { get; set; }
+    }
+}
---
+++ TicketManager/TicketManager.DataAccess.Notifications/NotificationsContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketManager.DataAccess.Notifications.DataModel;
+
+namespace TicketManager.DataAccess.Notifications
+{
+    public class NotificationsContext : DbContext
+    {
+        public NotificationsContext(DbContextOptions<NotificationsContext> dbContextOptions)
+            : base(dbContextOptions)
+        {
+        }
+
+        public DbSet<Notification> Notifications { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // TODO: Configure entities
+        }
+    }
+}
