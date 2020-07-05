c855074fedd4e6d2ba327d18ca40ab075c614a56
BalazsArva <balazs.arva@windowslive.com>
2020-07-05 12:36:17 +0200

    Created startup tasks for creating rabbitmq exchanges and exchange-bound queues.

---
+++ TicketManager/TicketManager.Messaging/StartupTasks/CreateRabbitMqExchange.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
+using TicketManager.Common.StartupTasks.Abstractions;
+using TicketManager.Messaging.Configuration;
+
+namespace TicketManager.Messaging.StartupTasks
+{
+    public class CreateRabbitMqExchange : IApplicationStartupTask
+    {
+        private readonly RabbitMqExchangeConfiguration options;
+
+        public CreateRabbitMqExchange(IOptions<RabbitMqExchangeConfiguration> options)
+        {
+            this.options = options?.Value ?? throw new ArgumentNullException(nameof(options));
+        }
+
+        public Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = options.HostName,
+                AutomaticRecoveryEnabled = true,
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(options.ExchangeName, ExchangeType.Fanout, true, false, null);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ TicketManager/TicketManager.Messaging/StartupTasks/CreateRabbitMqExchangeBoundQueue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
+using TicketManager.Common.StartupTasks.Abstractions;
+using TicketManager.Messaging.Configuration;
+
+namespace TicketManager.Messaging.StartupTasks
+{
+    public class CreateRabbitMqExchangeBoundQueue : IApplicationStartupTask
+    {
+        private readonly RabbitMqExchangeBoundQueueConfiguration options;
+
+        public CreateRabbitMqExchangeBoundQueue(IOptions<RabbitMqExchangeBoundQueueConfiguration> options)
+        {
+            this.options = options?.Value ?? throw new ArgumentNullException(nameof(options));
+        }
+
+        public Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = options.HostName,
+                AutomaticRecoveryEnabled = true,
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueBind(options.QueueName, options.ExchangeName, string.Empty, null);
+
+            return Task.CompletedTask;
+        }
+    }
+}
