f4eae661eea04ad0f63a661003150589f096cf03
BalazsArva <balazs.arva@windowslive.com>
2020-07-05 12:34:55 +0200

    Created IApplicationStartup in the common lib.

---
+++ TicketManager/TicketManager.Common/StartupTasks/Abstractions/IApplicationStartupTask.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace TicketManager.Common.StartupTasks.Abstractions
+{
+    public interface IApplicationStartupTask
+    {
+        Task ExecuteAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ TicketManager/TicketManager.Common/Utils/Throw.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TicketManager.Common.Utils
+{
+    public static class Throw
+    {
+        public static void IfNull<T>(string parameterName, T parameterValue)
+            where T : class
+        {
+            if (parameterValue is null)
+            {
+                throw new ArgumentNullException(parameterName, $"The value of the '{parameterName}' cannot be null.");
+            }
+        }
+
+        public static void IfNullOrWhiteSpace(string parameterName, string parameterValue)
+        {
+            if (string.IsNullOrWhiteSpace(parameterValue))
+            {
+                throw new ArgumentException($"The value of the '{parameterName}' cannot be null, empty or whitespace-only.", parameterName);
+            }
+        }
+    }
+}
