0e63af5103d895c4c07b176f6e4b3dbdc018a6eb
BalazsArva <balazs.arva@windowslive.com>
2020-07-05 18:56:02 +0200

    Moved DB setup tasks to their respective projects.

---
+++ TicketManager/TicketManager.DataAccess.Documents/StartupTasks/SetupDocumentsDatabase.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Operations;
+using Raven.Client.Exceptions;
+using Raven.Client.Exceptions.Database;
+using Raven.Client.ServerWide;
+using Raven.Client.ServerWide.Operations;
+using TicketManager.Common.StartupTasks.Abstractions;
+
+namespace TicketManager.DataAccess.Documents.StartupTasks
+{
+    public class SetupDocumentsDatabase : IApplicationStartupTask
+    {
+        private readonly IConfiguration configuration;
+        private readonly IDocumentStore documentStore;
+
+        public SetupDocumentsDatabase(IConfiguration configuration, IDocumentStore documentStore)
+        {
+            this.configuration = configuration;
+            this.documentStore = documentStore ?? throw new ArgumentNullException(nameof(documentStore));
+        }
+
+        public Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            EnsureDatabaseExists();
+            CreateIndexes();
+
+            return Task.CompletedTask;
+        }
+
+        private void EnsureDatabaseExists()
+        {
+            var database = configuration["DataAccess:RavenDb:Database"];
+
+            try
+            {
+                documentStore.Maintenance.ForDatabase(database).Send(new GetStatisticsOperation());
+            }
+            catch (DatabaseDoesNotExistException)
+            {
+                try
+                {
+                    documentStore.Maintenance.Server.Send(new CreateDatabaseOperation(new DatabaseRecord(database), 1));
+                }
+                catch (ConcurrencyException)
+                {
+                    // The database was already created before calling CreateDatabaseOperation
+                }
+            }
+        }
+
+        private void CreateIndexes()
+        {
+            IndexCreator.CreateIndexes(documentStore);
+        }
+    }
+}
---
+++ TicketManager/TicketManager.DataAccess.Events/StartupTasks/MigrateEventsDatabase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TicketManager.Common.StartupTasks.Abstractions;
+
+namespace TicketManager.DataAccess.Events.StartupTasks
+{
+    public class MigrateEventsDatabase : IApplicationStartupTask
+    {
+        private readonly IConfiguration configuration;
+        private readonly IEventsContextFactory eventsContextFactory;
+
+        public MigrateEventsDatabase(IConfiguration configuration, IEventsContextFactory eventsContextFactory)
+        {
+            this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            this.eventsContextFactory = eventsContextFactory ?? throw new ArgumentNullException(nameof(eventsContextFactory));
+        }
+
+        public async Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            using var context = eventsContextFactory.CreateContext();
+
+            await context.Database.MigrateAsync();
+        }
+    }
+}
