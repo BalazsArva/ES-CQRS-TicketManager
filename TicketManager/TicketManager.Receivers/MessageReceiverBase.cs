718dcdd66710fb13702bb78768691359c458c2e2
BalazsArva <balazs.arva@windowslive.com>
2020-07-12 12:21:16 +0200

    Work on rabbitmq-based message receiver background service.

--- TicketManager/TicketManager.Receivers/MessageReceiverBase.cs
+++ TicketManager/TicketManager.Receivers/MessageReceiverBase.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using TicketManager.Common.Http;
 using TicketManager.Messaging.Configuration;
 
 namespace TicketManager.Receivers
@@ -12,13 +16,17 @@ namespace TicketManager.Receivers
     public abstract class MessageReceiverBase<TMessage> : BackgroundService
     {
         private readonly RabbitMqExchangeBoundQueueConfiguration options;
+        private readonly ILogger logger;
 
         private IModel channel;
         private IConnection connection;
+
+        // TODO: Implement disposal
         private bool disposed;
 
-        protected MessageReceiverBase(IOptions<RabbitMqExchangeBoundQueueConfiguration> options)
+        protected MessageReceiverBase(ILogger<MessageReceiverBase<TMessage>> logger, IOptions<RabbitMqExchangeBoundQueueConfiguration> options)
         {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
             this.options = options?.Value ?? throw new ArgumentNullException(nameof(options));
 
             Initialize();
@@ -26,14 +34,11 @@ namespace TicketManager.Receivers
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            var consumer = new EventingBasicConsumer(channel);
+            var consumer = new AsyncEventingBasicConsumer(channel);
 
-            consumer.Received += MessageReceived;
+            consumer.Received += async (sender, eventArgs) => await MessageReceivedAsync(eventArgs, stoppingToken);
 
-            channel.BasicConsume(
-                queue: options.QueueName,
-                autoAck: false,
-                consumer: consumer);
+            var consumerTag = channel.BasicConsume(options.QueueName, false, consumer);
 
             try
             {
@@ -41,16 +46,37 @@ namespace TicketManager.Receivers
             }
             catch (TaskCanceledException)
             {
-                consumer.Received -= MessageReceived;
+                channel.BasicCancel(consumerTag);
 
-                // TODO: Stop receiving, shutdown gracefully.
+                // TODO: Stop receiving, shutdown gracefully. Check if event handler removal is needed.
             }
         }
 
-        private void MessageReceived(object sender, BasicDeliverEventArgs e)
+        protected abstract Task ProcessMessageAsync(TMessage message, CancellationToken cancellationToken);
+
+        private async Task MessageReceivedAsync(BasicDeliverEventArgs e, CancellationToken stoppingToken)
         {
-            // TODO: Implement
-            Console.WriteLine("Message received");
+            try
+            {
+                if (!string.Equals(e.BasicProperties.ContentEncoding, Encoding.UTF8.HeaderName, StringComparison.OrdinalIgnoreCase) ||
+                    !string.Equals(e.BasicProperties.ContentType, StandardContentTypes.Json, StringComparison.OrdinalIgnoreCase))
+                {
+                    // TODO: Signal error
+                    return;
+                }
+
+                var messageBodyJson = Encoding.UTF8.GetString(e.Body.Span);
+                var messageBody = JsonConvert.DeserializeObject<TMessage>(messageBodyJson);
+
+                await ProcessMessageAsync(messageBody, stoppingToken);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(
+                    ex,
+                    "An unhandled error occurred while trying to process message with Id='{MessageId}'.",
+                    e.BasicProperties.MessageId);
+            }
         }
 
         private void Initialize()
@@ -59,6 +85,7 @@ namespace TicketManager.Receivers
             {
                 HostName = options.HostName,
                 AutomaticRecoveryEnabled = true,
+                DispatchConsumersAsync = true,
             };
 
             connection = factory.CreateConnection();
